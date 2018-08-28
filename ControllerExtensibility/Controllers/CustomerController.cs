b6e9dbf065d50283029bf4309a4d3620cc9821a5
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-28 10:35:36 +0400

    Создание простого проекта

---
+++ ControllerExtensibility/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using ControllerExtensibility.Models;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "Index"
+            });
+        }
+
+        public ActionResult List()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "List"
+            });
+        }
+    }
+}
---
+++ ControllerExtensibility/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ControllerExtensibility.Models;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Product",
+                ActionName = "Index"
+            });
+        }
+
+        public ActionResult List()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Product",
+                ActionName = "List"
+            });
+        }
+    }
+}
