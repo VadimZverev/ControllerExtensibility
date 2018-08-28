df6bb9e0c68bab278daa01a67420f1248ffb81d8
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-28 12:41:34 +0400

    Пользовательский механизм вызова действий

---
+++ ControllerExtensibility/Infrastructure/CustomActionInvoker.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    public class CustomActionInvoker : IActionInvoker
+    {
+        public bool InvokeAction(ControllerContext controllerContext, string actionName)
+        {
+            if (actionName == "Index")
+            {
+                controllerContext.HttpContext.
+                    Response.Write("Это выводится из действия Index");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
