561e540455a7267c3604d43fdc2c2816d5e06f84
Вадим Зверев <WadimZverev@gmail.com>
2018-08-29 01:05:57 +0400

    Создаем пользовательский селектор метода действия

---
+++ ControllerExtensibility/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ControllerExtensibility.Infrastructure;
+using ControllerExtensibility.Models;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Home",
+                ActionName = "Index"
+            });
+        }
+
+        [Local]
+        [ActionName("Index")]
+        public ActionResult LocalIndex()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Home",
+                ActionName = "LocalIndex"
+            });
+        }
+    }
+}
