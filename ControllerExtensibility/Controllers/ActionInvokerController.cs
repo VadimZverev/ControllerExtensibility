49fd846b1bcd44f33be6619615911df218471e9c
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-28 12:52:51 +0400

    Используем пользовательский механизм вызова действий в контроллере

---
+++ ControllerExtensibility/Controllers/ActionInvokerController.cs
@@ -0,0 +1,13 @@
+﻿using ControllerExtensibility.Infrastructure;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class ActionInvokerController : Controller
+    {
+        public ActionInvokerController()
+        {
+            ActionInvoker = new CustomActionInvoker();
+        }
+    }
+}
