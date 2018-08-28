551bd9ac829e1abefa1d450896ad43a7fa7bfb9d
Вадим Зверев <WadimZverev@gmail.com>
2018-08-29 02:18:51 +0400

    Создаем пример проблемного синхронного контроллера

---
+++ ControllerExtensibility/Models/RemoteService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace ControllerExtensibility.Models
+{
+    public class RemoteService
+    {
+        public string GetRemoteData()
+        {
+            Thread.Sleep(2000);
+            return "Привет с другой стороны мира";
+        }
+    }
+}
