fb03ca0d2ee9a8d136b9922537e9496fddbbb639
Вадим Зверев <WadimZverev@gmail.com>
2018-08-29 02:01:27 +0400

    Управляем состоянием сессии с помощью DefaultControllerFactory

---
+++ ControllerExtensibility/Controllers/FastController.cs
@@ -0,0 +1,19 @@
+﻿using ControllerExtensibility.Models;
+using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace ControllerExtensibility.Controllers
+{
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class FastController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Fast",
+                ActionName = "Index"
+            });
+        }
+    }
+}
